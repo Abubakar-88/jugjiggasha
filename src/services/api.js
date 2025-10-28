6375fabbedf8117f27d0a6666d9dcd213cadc792
Abu Bakar <siddique0153@gmail.com>
2025-10-28 23:30:53 +0600

    api link updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost/Jugjiggasha/api';
+const API_BASE_URL = 'http://pale-twyla-abubakar-app-c11000c5.koyeb.app/Jugjiggasha/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
