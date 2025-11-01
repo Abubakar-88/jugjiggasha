2f9a1efe78e567e30e854572ec12fde72352d9ac
Abu Bakar <siddique0153@gmail.com>
2025-11-01 22:41:27 +0600

    auto updated feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
